6046cd7774bdbb7c2e7ea800fa3dfc088936a492
Nikola Kojadinovic <nikolakojadinovickoja@gmail.com>
2022-05-06 21:18:13 +0200

    Automated task initial commit

---
+++ automatedTask/src/main/java/com/eSystems/automatedTask/AutomatedTaskApplication.java
@@ -0,0 +1,13 @@
+package com.eSystems.automatedTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTaskApplication.class, args);
+	}
+
+}
